83f453a57ec99cc85c92f2aba08f275f8420d03f
Shravya Patel <shravyapatel2806@gmail.com>
2025-08-15 15:17:32 +0530

    Add files via upload

---
+++ dataTypesAndVariables.java
@@ -0,0 +1,42 @@
+class dataTypesAndVariables {
+    public static void main(String[] args) {
+       int x = 10;
+       int y = 20;
+       System.out.println("x = "+x+"\ny = "+y);
+       boolean t = true;
+       float f = 3.14f;
+       double d = 32.456;
+       byte b = 127;
+       b = -128;
+       x = Integer.MAX_VALUE;
+       System.out.println("x = "+x);
+       x = Integer.MIN_VALUE;
+       System.out.println("x = "+x);
+       long l = Long.MAX_VALUE;
+       System.out.println("l = "+l);
+       f = Float.MAX_EXPONENT; 
+       System.out.println("f = "+f);
+       d = Double.MAX_EXPONENT;
+       System.out.println("d = "+d);
+       char c = 'k';
+        // secondary data types
+        String  s = "My new Subscriber is John";
+//         There are some rules to declare variables
+// Rule 1 :-  Variable name can't be start with numbers . For example :- int 12hello = 18 ; this is wrong way to declare variable
+// Rule 2 :- it should not contain any special character except $ and _ . For example 
+// Rule 3 :- if you want to name the variable continues name then use camel case writing . For example int newVariable = 18;
+// Rule 4 :- Java is case-sensitive, so Name and name are different
+// Rule 5 :- Use meaningful names, like userAge instead of just x
+    final float pi = 3.14f;
+    // int int = 10;
+    // do, whlie ,for ,else, if ,else if, int , doiuble ,etc
+    // pi = 12.3;
+    int radius = 2;
+    float area = pi*radius*radius;
+       System.out.println("area = "+area);
+    
+int _1223 = 13;
+int new_variable = 15;
+
+    }
+}
