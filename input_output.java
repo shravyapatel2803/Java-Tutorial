5309430460ea01e6106211bd36089d454dc3e44c
sharvya2803 <shravyapatel2806@gmail.com>
2025-10-07 13:28:04 +0530

    new file input and output

---
+++ input_output.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class input_output {
+    public static void main(String[] args) {
+        // How we can take inputs from console
+        Scanner scan = new Scanner(System.in);
+////        int number = scan.nextInt();
+////        System.out.println(number);
+//        int number = scan.nextInt();
+//        scan.nextLine();
+//        String s = scan.nextLine();
+//        String s2 = scan.nextLine();
+//        System.out.println(s);
+//        System.out.println(number);
+//        System.out.println(s2);
+//        float f = scan.nextFloat();
+//        System.out.println(f);
+//        double d = scan.nextDouble();
+//        System.out.println(d);
+
+
+        ///  output
+
+        System.out.print(155351);
+        System.out.println(" "+12886);
+    }
+}
